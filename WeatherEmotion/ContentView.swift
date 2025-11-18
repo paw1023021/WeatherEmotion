783c89ad722cd1d449bb259673e1087e5a82106c
paw1023021 <paw102796@gmail.com>
2025-11-18 15:47:17 +0900

    Initial Commit

---
+++ WeatherEmotion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherEmotion
+//
+//  Created by dsu_student on 11/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherEmotion/WeatherEmotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherEmotionApp.swift
+//  WeatherEmotion
+//
+//  Created by dsu_student on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherEmotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
